b6256d2a88eb8e5a80a696dc4a6edc80d5f05721
biezhi <biezhi.me@gmail.com>
2018-06-07 17:26:48 +0800

    :bug: fixed jackson java8 time

--- blade-jackson-support/src/main/java/com/blade/kit/json/JacksonSupport.java
+++ blade-jackson-support/src/main/java/com/blade/kit/json/JacksonSupport.java
@@ -2,6 +2,10 @@ package com.blade.kit.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -18,6 +22,15 @@ public class JacksonSupport implements JsonSupport {
 
     public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+    static {
+        OBJECT_MAPPER
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+
+        OBJECT_MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
     @Override
     public String toString(Object obj) {
         try {
@@ -32,7 +45,7 @@ public class JacksonSupport implements JsonSupport {
     public <T> T formJson(String value, Type type) {
         T object = null;
         try {
-            object = (T) OBJECT_MAPPER.readValue(value, type.getClass());
+            object = (T) OBJECT_MAPPER.readValue(value, (Class) type);
         } catch (IOException e) {
             log.error("Jackson readValue fail", e);
         }
